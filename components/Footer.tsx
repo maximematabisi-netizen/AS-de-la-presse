aa694fe0032a85f4a446a19deb378362b2d3bc25
Maxime <maximematabisi@gmail.com>
2025-11-07 12:08:31 +0100

    chore(ui): update YouTube link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -71,7 +71,7 @@ const Footer = () => {
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                 </svg>
               </a>
-              <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" aria-label="YouTube" className="social-icon social-icon-youtube social-icon-animate text-gray-400 hover:text-white">
+              <a href="https://www.youtube.com/@lesasdelapresse7865" target="_blank" rel="noopener noreferrer" aria-label="YouTube" className="social-icon social-icon-youtube social-icon-animate text-gray-400 hover:text-white">
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden>
                   <path d="M23.498 6.186a2.999 2.999 0 00-2.117-2.117C19.73 3.5 12 3.5 12 3.5s-7.73 0-9.381.569A2.999 2.999 0 00.5 6.186 31.78 31.78 0 000 12a31.78 31.78 0 00.5 5.814 2.999 2.999 0 002.119 2.117C4.27 20.5 12 20.5 12 20.5s7.73 0 9.381-.569a2.999 2.999 0 002.117-2.117A31.78 31.78 0 0024 12a31.78 31.78 0 00-.502-5.814zM9.75 15.02V8.98L15.5 12l-5.75 3.02z" />
                 </svg>
